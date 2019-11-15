99bf28279071b408c7d41527f3d3b66b45d3b154
Cody Johnson <codyj@protonmail.com>
2019-11-15 11:17:02 -0800

    Install ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 
@@ -13,6 +14,7 @@ import {NewsModule} from './news/news.module';
   ],
   imports: [
     BrowserModule,
+    NgbModule,
     AppRoutingModule,
     NavigationModule,
     NewsModule
