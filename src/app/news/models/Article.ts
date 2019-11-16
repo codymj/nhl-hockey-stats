443cf4dc2d0ab185d6ca2a688831426b90428470
Cody Johnson <codyj@protonmail.com>
2019-11-16 07:56:28 -0800

    Add Article model

--- src/app/news/models/Article.ts
+++
@@ -1,5 +0,0 @@
-export class Article {
-  imgUrl: string;
-  headline: string;
-  description: string;
-}
---
+++ src/app/news/models/article.model.ts
@@ -0,0 +1,7 @@
+export class Article {
+  constructor(
+    imgUrl: string,
+    headline: string,
+    description: string
+  ) {}
+}
