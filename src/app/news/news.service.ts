3e42e0c4e2f93234543ae6280be6fb33ab2884ee
Cody Johnson <codyj@protonmail.com>
2019-11-16 07:57:07 -0800

    Initialize News service

--- src/app/news/news.service.ts
+++ src/app/news/news.service.ts
@@ -1,9 +1,24 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService {
+  private BASE_URL = 'http://site.api.espn.com/apis/site/v2/sports/hockey/nhl/news';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // Gets entire news JSON from endpoint
+  private getNews(): Observable<any> {
+    return this.http.get(this.BASE_URL);
+  }
+
+  // Parse the articles and return
+  public getArticles() {
+    this.getNews().subscribe(res => {
+      return res.articles;
+    });
+  }
 }
