cc99ce35da17c6efe22bb62fc17767ee85f8ab22
Cody Johnson <codyj@protonmail.com>
2019-11-18 11:24:03 -0800

    Complete Article model, modify parsing of articles

--- src/app/news/carousel/carousel.component.ts
+++ src/app/news/carousel/carousel.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {NewsService} from '../news.service';
+import {Article} from '../models/article.model';
 
 @Component({
   selector: 'app-carousel',
@@ -7,8 +8,7 @@ import {NewsService} from '../news.service';
   styleUrls: ['./carousel.component.css']
 })
 export class CarouselComponent implements OnInit {
-  private fallbackImgUrl = 'https://i.imgur.com/ETAC3fC.jpg';
-  private articles: [];
+  private articles: Article[];
 
   constructor(private newsService: NewsService) { }
 
@@ -19,7 +19,21 @@ export class CarouselComponent implements OnInit {
   // Parse the articles from news service
   public getArticles() {
     this.newsService.getNews().subscribe(res => {
-      this.articles = res.articles;
+      // Clear array
+      this.articles = [];
+
+      // Parse JSON into Article objects
+      for (const article of res.articles) {
+        let imgUrl = '';
+        if (article.images.length > 0 && article.images[0].width > article.images[0].height) {
+          imgUrl = article.images[0].url;
+        }
+
+        const headline = article.headline;
+        const description = article.description;
+
+        this.articles.push(new Article(imgUrl, headline, description));
+      }
     });
   }
 }
--- src/app/news/models/article.model.ts
+++ src/app/news/models/article.model.ts
@@ -1,7 +1,23 @@
 export class Article {
-  constructor(
-    imgUrl: string,
-    headline: string,
-    description: string
-  ) {}
+  private imgUrl: string;
+  private headline: string;
+  private description: string;
+
+  constructor(imgUrl: string, headline: string, description: string) {
+    this.imgUrl = imgUrl;
+    this.headline = headline;
+    this.description = description;
+  }
+
+  public getImgUrl(): string {
+    return this.imgUrl;
+  }
+
+  public getHeadline(): string {
+    return this.headline;
+  }
+
+  public getDescription(): string {
+    return this.description;
+  }
 }
