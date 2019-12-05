6c8bf88df849f1b7c4c9d7001a69aea1ed4f9669
Cody Johnson <codyj@protonmail.com>
2019-12-05 14:02:48 -0800

    Revert "Edit table heading colors"
    Finalize standings service

--- src/app/standings/standings.service.ts
+++ src/app/standings/standings.service.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,15 +10,24 @@ export class StandingsService {
 
   constructor(private http: HttpClient) { }
 
+  // Fetches standings JSON by mode (e.g. byDivision or byConference)
+  private getStandings(modifier: string): Observable<any> {
+    return this.http.get(this.BASE_URL + modifier);
+  }
+
   // Returns divisional standings
-  public getStandingsByDivisions(): Observable<any> {
-    return this.http.get(this.BASE_URL + 'byDivision').pipe(map(res => {
-      return res;
-    }));
+  public getByDivisions() {
+    const divisions = [];
+    this.getStandings('byDivision').subscribe(res => {
+      for (const division of res.records) {
+        divisions.push(division);
+      }
+    });
+    return divisions;
   }
 
   // Returns conference standings
-  /*public getByConferences() {
+  public getByConferences() {
     const conferences = [];
     this.getStandings('byConference').subscribe(res => {
       for (const conference of res.records) {
@@ -27,5 +35,5 @@ export class StandingsService {
       }
     });
     return conferences;
-  }*/
+  }
 }
