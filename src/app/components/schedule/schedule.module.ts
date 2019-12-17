7b1e749a36e6f86fdf30ab7349949db02a7cc99f
Cody Johnson <codyj@protonmail.com>
2019-12-17 11:26:09 -0800

    Some style edits

--- src/app/components/schedule/schedule.module.ts
+++ src/app/components/schedule/schedule.module.ts
@@ -3,9 +3,10 @@ import {CommonModule} from '@angular/common';
 import {MainScheduleComponent} from './main-schedule/main-schedule.component';
 import {ScheduleElementComponent} from './schedule-element/schedule-element.component';
 import {MainScoresComponent} from './main-scores/main-scores.component';
+import {ScoreElementComponent} from './score-element/score-element.component';
 
 @NgModule({
-  declarations: [MainScheduleComponent, ScheduleElementComponent, MainScoresComponent],
+  declarations: [MainScheduleComponent, ScheduleElementComponent, MainScoresComponent, ScoreElementComponent],
   exports: [
     MainScheduleComponent,
     MainScoresComponent
